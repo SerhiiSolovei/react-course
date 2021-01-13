2ba8bf940c7ab411af4bfe445017810180f578eb
Your Name <you@example.com>
2021-01-13 22:17:53 +0200

    Example of importing CSS and working with CSS styles in React components

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  AppHeader from './components/app-header';
-import  TodoList from './components/todo-list';
-import  SearchPanel from './components/search-panel';
 
-/*
-  const el = React.createElement('h1', null, 'Hello World!!!'); // JS code
-  const el = <h1>Hello World!</h1>; // JSX code which converted to backwards compatible version of JavaScript using Babel transcompiler
-  console.log(typeof el); // object
-*/
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
-// Create components
+import './index.css';
 
 const App = () => {
 
   const todoData = [
-    {label: 'Drink Coffee', important: false, id: 1},
-    {label: 'Make Awesome App', important: true, id: 2},
-    {label: 'Have a lunch', important: false, id: 3}
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
   ];
 
   return (
-  <div>
-    <AppHeader />
-    <SearchPanel />
-    <TodoList todos = {todoData}/>
-  </div>
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
