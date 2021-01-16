8974e7999438536f8ab5a77f599518c3860b2616
Your Name <you@example.com>
2021-01-16 17:47:18 +0200

    TodoList Project: search panel implementation

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends React.Component {
       this.createTodoItem('Drink Coffee'),
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch')
-    ]
+    ],
+    term: ''
   };
 
   createTodoItem (label) {
@@ -113,13 +114,27 @@ export default class App extends React.Component {
       //   todoData: newArr
       // }
     });
+  };
 
+  onSearchChange = (term) => {
+    return this.setState({ term });
+  };
 
+  search (items, term) {
+    if (term.length === 0) {
+      return items
+    }
+
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+    });
   };
 
   render () {
 
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
+
+    const visibleItems = this.search(todoData, term);
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
 
@@ -127,12 +142,13 @@ export default class App extends React.Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel
+            onSearchChange={this.onSearchChange}/>
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          todos={todoData}
+          todos={visibleItems}
           onDeleted = { this.deleteItem }
           onToggleImportant = {this.onToggleImportant }
           onToggleDone = { this.onToggleDone }/>
