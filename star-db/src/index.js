aabbe9e83744dd209d0e399f5a0de45a89de1130
Your Name <you@example.com>
2021-01-17 17:38:36 +0200

    Examples of handling error in Fetch API

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,10 +1,16 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    console.log(`Could not fetch ${url}, received ${res.status}`);
+  };
+
   const body = await res.json();
-  console.log(res, body);
+
   return body;
 }
 
 
-getResource('https://swapi.dev/api/people/1')
-  .then((body)=> console.log(body));
+getResource('https://swapi.dev/api/people/112323123')
+  .then((body)=> console.log(body))
+  .catch((err) => console.log('Could not fetch', err));
