e7188b5b1ec20da030ac9597662a29bb45e87a44
Your Name <you@example.com>
2021-01-17 17:21:19 +0200

    Examples of using Fetch API

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  console.log(res, body);
+  return body;
+}
+
+
+getResource('https://swapi.dev/api/people/1')
+  .then((body)=> console.log(body));
