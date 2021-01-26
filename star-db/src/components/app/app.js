8a2599cc30f22d2b96e0dd9bc9d9aa522a79dba7
Your Name <you@example.com>
2021-01-26 18:43:31 +0200

    StarDB Project: examples of using componentDidCatch() method for different error boundaries and adding some structural changes

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
+
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
@@ -22,13 +24,17 @@ export default class App extends Component {
     });
   };
 
-  onSelectedPerson = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
+
+  componentDidCatch() {
+    console.log('componentDidCatch()');
+    this.setState({ hasError: true});
   };
 
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    };
 
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
@@ -39,20 +45,18 @@ export default class App extends Component {
         <Header />
         { planet }
 
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}>
-          Toggle Random Planet
-        </button>
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onSelectedPerson}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
         </div>
+
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
       </div>
     );
   };
