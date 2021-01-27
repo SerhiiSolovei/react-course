bd9879ccad8c72880edc4c99375927dc6ec837d7
Your Name <you@example.com>
2021-01-27 21:54:32 +0200

    StarDB Project: examples of using property-element (React Pattern) and making some structural changes

--- star-db/src/components/people-page/people-page.js
+++ star-db/src/components/people-page/people-page.js
@@ -3,6 +3,7 @@ import SwapiService from '../../services/swapi-service';
 import ErrorIndicator from '../error-indicator';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import Row from '../row';
 
 import './people-page.css';
 
@@ -33,18 +34,21 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     };
 
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onSelectedPerson}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name, gender, birthYear }) => (`${name} (${gender}, ${birthYear})`)}/>
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson}/>
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onSelectedPerson}
-            getData={this.swapiService.getAllPeople}
-              renderItem={({ name, gender, birthYear }) => (`${name} (${gender}, ${birthYear})`)}/>
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson}/>
-        </div>
-    </div>
+      <div>
+        <Row left={itemList} right={personDetails}/>
+      </div>
     );
   };
 };
