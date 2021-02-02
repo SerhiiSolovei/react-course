3345941441b01b055d07e12fcdd904b624c3f51a
Your Name <you@example.com>
2021-02-02 22:11:09 +0200

    StarDB Project: examples of using Context API

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ErrorButton from '../error-button';
+import ErrorBoundary from '../error-boundary';
 import ErrorIndicator from '../error-indicator';
-import PeoplePage from '../people-page';
-import ItemList from '../item-list';
 import ItemDetails, { Record } from '../item-details/item-details';
 import SwapiService from '../../services/swapi-service';
-import Row from '../row';
+import { SwapiServiceProvider } from '../swapi-service-context';
 
 import {
   PersonList,
@@ -20,7 +18,6 @@ import {
 
 
 import './app.css';
-import ErrorBoundary from '../error-boundary';
 
 export default class App extends Component {
 
@@ -82,52 +79,20 @@ export default class App extends Component {
     );
     return (
       <ErrorBoundary>
-      <div className="stardb-app">
-        <Header />
-        {/* { planet }
+        <SwapiServiceProvider value={this.swapiService}>
+          <div className="stardb-app">
+            <Header />
 
-        <div className="row mb2 button-row">
-          <button
-            className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet}>
-            Toggle Random Planet
-          </button>
-          <ErrorButton />
-        </div>
+            <PersonDetails itemId={11}/>
+            <PlanetDetails itemId={2}/>
+            <StarshipDetails itemId={9}/>
 
-        <PeoplePage /> */}
+            <PersonList />
+            <StarshipList />
+            <PlanetList />
 
-        {/* <div className="row mb2">
-          <div className="col-md-6"> */}
-
-          {/* </div> */}
-          {/* <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div> */}
-        {/* </div> */}
-
-        {/* <div className="row mb2">
-          <div className="col-md-6"> */}
-
-          <PersonDetails itemId={11}/>
-          <PlanetDetails itemId={2}/>
-          <StarshipDetails itemId={9}/>
-
-          <PersonList />
-
-          <StarshipList />
-
-          <PlanetList />
-
-          {/* </div> */}
-          {/* <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div> */}
-        {/* </div> */}
-
-        {/* <Row left={personDetails} right={starshipDetails}/> */}
-
-      </div>
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundary>
     );
   };
