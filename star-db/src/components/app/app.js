f900b58a98a0298aae9f20fb289b9920cb5176c1
Your Name <you@example.com>
2021-02-07 18:06:18 +0200

    StarDB Project: examples of using Redirect {react-router} Component for authorization system

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -7,7 +7,7 @@ import ErrorBoundary from '../error-boundary';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service'
 import { SwapiServiceProvider } from '../swapi-service-context';
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import { PeoplePage, PlanetsPage, StarshipsPage, LoginPage, SecretPage } from '../pages';
 import StarshipDetails from '../sw-components/starship-details'
 
 import './app.css';
@@ -15,7 +15,14 @@ import './app.css';
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    return this.setState({
+      isLoggedIn: true
+    })
   };
 
   onServiceChange =() => {
@@ -50,6 +57,19 @@ export default class App extends Component {
                   return <StarshipDetails itemId={id}/>
                 }
               }/>
+
+              <Route
+                path="/login"
+                render={() => (
+                  <LoginPage
+                    isLoggedIn={this.state.isLoggedIn}
+                    onLogin={this.onLogin}/>
+                )}/>
+              <Route
+                path="/secret"
+                render={() => (
+                  <SecretPage isLoggedIn={this.state.isLoggedIn}/>
+                )}/>
 
             </div>
           </Router>
