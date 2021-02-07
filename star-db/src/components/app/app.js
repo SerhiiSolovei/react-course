1b4aeec598dc09fa06b65d3ef53ed2efb3771c81
Your Name <you@example.com>
2021-02-07 21:08:52 +0200

    StarDB Project: examples of handling non-existent addresses using Switch (react-router) Component

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -45,32 +45,35 @@ export default class App extends Component {
               <Header onServiceChange={this.onServiceChange}/>
               <RandomPlanet />
 
-              <Route path="/" render={()=> <h2>Welcome to StarDB</h2>} exact/>
-              <Route path="/people" render={()=> <h2>People</h2>} exact/>
-              <Route path="/people/:id?" component={PeoplePage}/>
-              <Route path="/planets" component={PlanetsPage}/>
-              <Route path="/starships" component={StarshipsPage} exact/>
-              <Route path="/starships/:id" render={({match})=> {
+              <Switch>
+                <Route path="/" render={()=> <h2>Welcome to StarDB</h2>} exact/>
+                <Route path="/people/:id?" component={PeoplePage}/>
+                <Route path="/planets" component={PlanetsPage}/>
+                <Route path="/starships" component={StarshipsPage} exact/>
+                <Route path="/starships/:id" render={({match})=> {
 
-                  const { id } = match.params;
+                    const { id } = match.params;
 
-                  return <StarshipDetails itemId={id}/>
-                }
-              }/>
+                    return <StarshipDetails itemId={id}/>
+                  }
+                }/>
 
-              <Route
-                path="/login"
-                render={() => (
-                  <LoginPage
-                    isLoggedIn={this.state.isLoggedIn}
-                    onLogin={this.onLogin}/>
-                )}/>
-              <Route
-                path="/secret"
-                render={() => (
-                  <SecretPage isLoggedIn={this.state.isLoggedIn}/>
-                )}/>
+                <Route
+                  path="/login"
+                  render={() => (
+                    <LoginPage
+                      isLoggedIn={this.state.isLoggedIn}
+                      onLogin={this.onLogin}/>
+                  )}/>
+                <Route
+                  path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={this.state.isLoggedIn}/>
+                  )}/>
 
+                  {/* <Redirect to="/"/> */}
+                  <Route render={()=> <h2>Page not found</h2>}/>
+              </Switch>
             </div>
           </Router>
         </SwapiServiceProvider>
