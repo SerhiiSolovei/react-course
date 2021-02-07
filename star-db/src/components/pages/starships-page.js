751bf781722f9be122f40a9787f151dd5a35d657
Your Name <you@example.com>
2021-02-07 14:05:47 +0200

    StarDB Project: examples of using Relative Paths

--- star-db/src/components/pages/starships-page.js
+++ star-db/src/components/pages/starships-page.js
@@ -6,9 +6,7 @@ const  StarshipsPage = ({history}) => {
 
     return (
       <StarshipList
-        onItemSelected={(itemId) => {
-          history.push(`/starships/${itemId}`);
-        }}/>
+        onItemSelected={(itemId) => history.push(itemId)}/>
     )
 };
 
