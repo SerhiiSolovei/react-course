825debf2f7f723bc752fa59806d9b42fdd22cfc6
Your Name <you@example.com>
2021-02-08 21:06:24 +0200

    Redux Sandbox Project: example of Redux Store

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,3 +1,5 @@
+import { createStore }  from 'redux';
+
 const reducer = (state = 0, action) => {
 
   switch (action.type) {
@@ -8,8 +10,12 @@ const reducer = (state = 0, action) => {
   };
 };
 
-let state = reducer(undefined, {});
-state = reducer(state, { type: 'INC'});
-console.log(state);
-state = reducer(state, { type: 'INC'});
-console.log(state);
+const store = createStore(reducer);
+//store.subscribe(fn) - it will be called any time an action is dispatched
+store.subscribe(() => {
+  console.log(store.getState()); // store.getState() - using for read the current state
+});
+
+store.dispatch({type: 'INC'}); // store.dispatch(action) - handling new action
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
