3a0efa650287e61398bd458063e787464af50552
Your Name <you@example.com>
2021-02-14 15:39:03 +0200

    ReStore Project: example of dispatching actions to the Redux Store

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import BookListItem from '../book-list-item';
+import { withBookstoreService } from '../hoc';
+import { booksLoaded} from '../../actions';
+import { compose } from '../../utils';
+
 import './book-list.css';
 
 class BookList extends Component {
+
+  componentDidMount () {
+    // 1. receive data
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+
+    // 2. dispatch data to store
+    this.props.booksLoaded(data);
+  };
 
   render () {
     const { books } = this.props;
@@ -28,4 +43,10 @@ const mapStateToProps = (state) => {
   }
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({booksLoaded}, dispatch);
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps,mapDispatchToProps))(BookList)
