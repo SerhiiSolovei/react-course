7e6a7dde07739816df849b3cfec1b6192a3b09fa
Your Name <you@example.com>
2021-02-18 20:52:01 +0200

    ReStore Project: example of creating Component-Container

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -10,7 +10,21 @@ import { compose } from '../../utils';
 
 import './book-list.css';
 
-class BookList extends Component {
+const BookList = ({books}) => {
+ return (
+  <ul className="book-list">
+    {
+      books.map((book) => {
+        return (
+          <li key={book.id}><BookListItem book={book}/></li>
+        );
+      })
+    }
+  </ul>
+);
+};
+
+class BookListContainer extends Component {
 
   componentDidMount () {
     this.props.fetchBooks();
@@ -27,17 +41,7 @@ class BookList extends Component {
       return <ErrorIndicator />
     }
 
-    return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}><BookListItem book={book}/></li>
-            );
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books}/>
   };
 };
 
@@ -58,4 +62,4 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 
 export default compose(
   withBookstoreService(),
-  connect(mapStateToProps,mapDispatchToProps))(BookList)
+  connect(mapStateToProps,mapDispatchToProps))(BookListContainer)
