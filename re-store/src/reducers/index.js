103ce9700fc8182618f23596f7910f0aead231ed
Your Name <you@example.com>
2021-02-19 20:46:52 +0200

    ReStore Project: examples of updating elements in an array to ReduxStore

--- re-store/src/reducers/index.js
+++ re-store/src/reducers/index.js
@@ -6,6 +6,39 @@ const initialState = {
   orderTotal: 220
 };
 
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [
+      ...cartItems,
+      item
+    ]
+  } else {
+    return [
+      ...cartItems.slice(0, idx),
+      item,
+      ...cartItems.slice(idx + 1)
+    ]
+  }
+};
+
+const updateCartItem = (book, item) => {
+
+  if (item) {
+    return {
+      ...item,
+      count: item.count + 1,
+      total: item.total + book.price
+    };
+  } else {
+    return {
+      id: book.id,
+      title: book.title,
+      count: 1,
+      total: book.price
+    };
+  }
+}
+
 const reducer = (state = initialState, action) => {
 
   switch (action.type) {
@@ -36,18 +69,14 @@ const reducer = (state = initialState, action) => {
       case 'BOOK_ADDED_TO_CART':
         const bookId = action.payload;
         const book = state.books.find((book) => bookId === book.id);
-        const newItem = {
-          id: book.id,
-          name: book.title,
-          count: 1,
-          total: book.price
-        };
+        const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
+        const item = state.cartItems[itemIndex];
+
+        const newItem = updateCartItem(book, item)
+
         return {
-          ...state,
-          cartItems: [
-            ...state.cartItems,
-            newItem
-          ]
+            ...state,
+            cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
         }
 
     default:
