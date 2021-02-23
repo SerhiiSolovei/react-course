7ca09b5a9825d42398f0960b30c87867a78a7416
Your Name <you@example.com>
2021-02-23 22:15:37 +0200

    Build-Sandbox: example of transformation JSX-code

--- build-sandbox/src/main.js
+++ build-sandbox/src/main.js
@@ -1,11 +1,20 @@
-class App {
-  run = async (name) => {
-    console.log(`Hello ${name}`);
-    console.log([1,2,3,[4,5]].flat());
-  };
+// class App {
+//   run = async (name) => {
+//     console.log(`Hello ${name}`);
+//     console.log([1,2,3,[4,5]].flat());
+//   };
+// };
+
+// const app = new App();
+// app.run('World')
+//   .then(()=> console.log('done'))
+//   .catch(()=> console.log('Error'));
+
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <p>Hello World!</p>
 };
 
-const app = new App();
-app.run('World')
-  .then(()=> console.log('done'))
-  .catch(()=> console.log('Error'));
+ReactDOM.render(<App />, document.getElementById('root'));
