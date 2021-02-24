a9fc59a06a936031a433b68d5cd9bff2c7f58524
Your Name <you@example.com>
2021-02-24 21:38:25 +0200

    Build-Sandbox: examples of using Loaders for different file types

--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -4,13 +4,27 @@ module.exports = {
 
   module: {
     rules: [
+
+      // Loading images
       {
-        test: /\.png$/,
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [{
           loader: 'file-loader',
           options: {
             outputPath: 'images',
             name: '[name]-[sha1:hash:7].[ext]'
+          }
+        }]
+      },
+
+      // Loading fonts
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'fonts',
+            name: '[name].[ext]'
           }
         }]
       }
