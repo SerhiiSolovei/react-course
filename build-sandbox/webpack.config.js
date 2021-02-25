32c73786e7267df977d6bf5d9479db92e1cf7db3
Your Name <you@example.com>
2021-02-25 18:44:53 +0200

    Build-Sandbox(WebPack): example of using Mini CSS Extract Plugin

--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: "development",
@@ -40,7 +41,7 @@ module.exports = {
       // CSS loader
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
@@ -50,6 +51,9 @@ module.exports = {
       title: 'Hello World',
       buildTime: new Date().toISOString(),
       template: 'public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-[hash:8].css'
     })
   ]
 };
